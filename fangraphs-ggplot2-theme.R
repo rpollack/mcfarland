4ed316ab52b3991597363597a5c1a4a8a344c79c
Ryan Pollack <ryanpollack@Ryans-MacBook-Pro.local>
2016-12-12 08:58:36 -0600

    fixed typo

--- fangraphs-ggplot2-theme.R
+++ fangraphs-ggplot2-theme.R
@@ -5,7 +5,7 @@ fgt = theme(panel.background=element_rect(color="lightgrey",fill="white"),
             legend.text = element_text(family="Lato"), 
             legend.background = element_rect(fill="white"), 
             legend.key = element_rect(fill="white"), 
-            lot.title = element_text(family="Lato"), 
+            plot.title = element_text(family="Lato"), 
             panel.grid.major=element_line(size=.1,color="lightgrey"), 
             panel.grid.minor=element_line(size=0))
 
