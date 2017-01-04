45a18a9f075f7d1dd2c46806679f1ecd574fc22a
Ryan Pollack <ryanpollack@Ryans-MacBook-Pro.local>
2017-01-03 20:21:30 -0600

    updated to (mostly) get salaries for each year, including basic arb. projections, so I can more accurately determine teams' salary commitments in the future

--- get-player-payrolls-bbref.R
+++ get-player-payrolls-bbref.R
@@ -28,8 +28,8 @@ getPayrolls <- function(Tm) {
   
   if (!"2021" %in% names(tms)) {
     tms$`2021` <- NA
-  
-}
+    
+  }
   
   tms <- tms %>%
     filter(!Name %in% c("Name","","Arb Costs", "Arb Eligible", "Contract Options", 
@@ -37,22 +37,31 @@ getPayrolls <- function(Tm) {
                         "Payroll (no options)", "Payroll (options)", "Signed")) 
   
   
-  #can get the '2017' column and extract any integers in there! maybe?
+  
   
   tms_melt <- gather(tms, key = Year, value, -Name, -Age, -Yrs, -Acquired, -SrvTm, -Agent, -`Contract Status`)
   tms_melt <- tms_melt %>%
     filter(value != "") %>%
     mutate(Team = Tm) %>%
-    group_by(Team, Name) %>%
-    mutate(rank = row_number()) %>%
-    filter(rank == max(rank)) %>%
-    select(Team, Name, everything(), -rank)
+    #group_by(Team, Name) %>%
+    mutate(value = ifelse(grepl("Pre-Arb", value), 0.535, value),
+           value = ifelse(grepl("M", value), as.double(str_extract(value, "(?<=\\$)[:digit:]+\\.*[:digit:]*")), value),
+           value = ifelse(grepl("k", value), as.double(str_extract(value, "(?<=\\$)[:digit:]+")) / 1000, value)) %>%
+    arrange(Name, Year) %>%
+    mutate(value = ifelse(value == "Arb-1", 0.535, value),
+           value = ifelse(value == "Arb-2" | value == "Arb", (as.double(lag(value)) / .25) * .4, value),
+           value = ifelse(value == "Arb-3", (as.double(lag(value)) / .4) * .6, value),
+           value = ifelse(value == "Arb-4", (as.double(lag(value)) / .6) * .8, value))
+  #mutate(rank = row_number()) %>%
+  #filter(rank == max(rank)) %>%
+  #select(Team, Name, Age, Year, value)
   
-  print(tms_melt)
+  #print(tms_melt)
 }
 
 all_teams_payroll <- teamAbbr %>% 
   group_by(Tm) %>%
-  do(getPayrolls(.$Tm))
-  
-  #write_csv(all_teams_payroll, "all_teams_payroll.csv")
+  do(getPayrolls(.$Tm)) %>%
+  select(Team, Name, Age, Year, value)
+
+#write_csv(all_teams_payroll, "all_teams_payroll.csv")
