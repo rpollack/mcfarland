62754a4a30839f05dc22bc82d419eca926a745fe
Ryan Pollack <ryanpollack@Ryans-MacBook-Pro.local>
2017-01-03 22:00:07 -0600

    removing commented-out code and unneeded whitespace

--- get-player-payrolls-bbref.R
+++ get-player-payrolls-bbref.R
@@ -1,6 +1,6 @@
 
-#shared by bill petti in FG slack chat on 11.25.2016
-
+# shared by bill petti in FG slack chat on 11.25.2016
+# modified by ryan pollack on 1.3.2017 to get detailed payroll information 
 
 getTeamabbr <- function(year) {
   team_tbl <- read_html(paste0("http://www.baseball-reference.com/leagues/MLB/", year, ".shtml"), stringsAsFactors=FALSE)
@@ -36,14 +36,10 @@ getPayrolls <- function(Tm) {
                         "Dollars Committed", "Other Costs", "Option Values", "Other Players", 
                         "Payroll (no options)", "Payroll (options)", "Signed")) 
   
-  
-  
-  
   tms_melt <- gather(tms, key = Year, value, -Name, -Age, -Yrs, -Acquired, -SrvTm, -Agent, -`Contract Status`)
   tms_melt <- tms_melt %>%
     filter(value != "") %>%
     mutate(Team = Tm) %>%
-    #group_by(Team, Name) %>%
     mutate(value = ifelse(grepl("Pre-Arb", value), 0.535, value),
            value = ifelse(grepl("M", value), as.double(str_extract(value, "(?<=\\$)[:digit:]+\\.*[:digit:]*")), value),
            value = ifelse(grepl("k", value), as.double(str_extract(value, "(?<=\\$)[:digit:]+")) / 1000, value)) %>%
@@ -52,16 +48,9 @@ getPayrolls <- function(Tm) {
            value = ifelse(value == "Arb-2" | value == "Arb", (as.double(lag(value)) / .25) * .4, value),
            value = ifelse(value == "Arb-3", (as.double(lag(value)) / .4) * .6, value),
            value = ifelse(value == "Arb-4", (as.double(lag(value)) / .6) * .8, value))
-  #mutate(rank = row_number()) %>%
-  #filter(rank == max(rank)) %>%
-  #select(Team, Name, Age, Year, value)
-  
-  #print(tms_melt)
 }
 
 all_teams_payroll <- teamAbbr %>% 
   group_by(Tm) %>%
   do(getPayrolls(.$Tm)) %>%
   select(Team, Name, Age, Year, value)
-
-#write_csv(all_teams_payroll, "all_teams_payroll.csv")
