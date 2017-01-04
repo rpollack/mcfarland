1befebd0867c7ba5daf76d8565f6a92e2fa04b09
Ryan Pollack <ryanpollack@Ryans-MacBook-Pro.local>
2017-01-03 22:24:44 -0600

    adding more comments so i remember what the hell this does

--- get-player-payrolls-bbref.R
+++ get-player-payrolls-bbref.R
@@ -32,20 +32,26 @@ getPayrolls <- function(Tm) {
   }
   
   tms <- tms %>%
+    
+    #remove unused cells in the 'Name' column
     filter(!Name %in% c("Name","","Arb Costs", "Arb Eligible", "Contract Options", 
                         "Dollars Committed", "Other Costs", "Option Values", "Other Players", 
                         "Payroll (no options)", "Payroll (options)", "Signed")) 
   
+  #for each player, create a single row, one for each "Year", where "value" is either their salary that year, their arbitration status, or their FA status, or blank if the player won't be on the payroll.
   tms_melt <- gather(tms, key = Year, value, -Name, -Age, -Yrs, -Acquired, -SrvTm, -Agent, -`Contract Status`)
+  
   tms_melt <- tms_melt %>%
     filter(value != "") %>%
     mutate(Team = Tm) %>%
+    
+    #give player minimum salary if they are pre-arb. else extract the actual or projected salary using a regex.
     mutate(value = ifelse(grepl("Pre-Arb", value), 0.535, value),
            value = ifelse(grepl("M", value), as.double(str_extract(value, "(?<=\\$)[:digit:]+\\.*[:digit:]*")), value),
            value = ifelse(grepl("k", value), as.double(str_extract(value, "(?<=\\$)[:digit:]+")) / 1000, value)) %>%
     arrange(Name, Year) %>%
     
-    #project arb years based on 25-40-60% formula located here: http://www.thepointofpittsburgh.com/calculating-mlb-arbitration-percentages/
+    #project future arb years based on 25-40-60% formula located here: http://www.thepointofpittsburgh.com/calculating-mlb-arbitration-percentages/
     #And project Arb-4 salaries at 80% of FA value per my own whims
     mutate(value = ifelse(value == "Arb-1", 0.535, value),
            value = ifelse(value == "Arb-2" | value == "Arb", (as.double(lag(value)) / .25) * .4, value),
